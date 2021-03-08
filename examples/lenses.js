466d3e5e65eb9f4a35cfad6128fc840c226951b7
Marcel Bos <bosmarcel@live.nl>
2021-03-08 14:31:40 +0100

    Delete lenses.js

--- examples/lenses.js
+++
@@ -1,7 +0,0 @@
-import { createLens } from "mobconx";
-// array
-export const friendsLens = createLens("userStore.friends", false);
-// computed
-export const bestFriendLens = createLens("userStore.bestFriend", true);
-// map
-export const hobbiesLens = createLens("userStore.hobbies", false);
